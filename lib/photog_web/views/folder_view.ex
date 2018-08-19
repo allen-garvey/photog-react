86dde305492e3808706809b770f02c67bd123f9c
Allen Garvey <allen.garvey@gmail.com>
2018-08-19 15:05:32 -0400

    Add list of albums with album cover image to api folder show view

--- lib/photog_web/views/folder_view.ex
+++ lib/photog_web/views/folder_view.ex
@@ -3,7 +3,7 @@ defmodule PhotogWeb.FolderView do
   alias PhotogWeb.FolderView
 
   def render("index.json", %{folders: folders}) do
-    %{data: render_many(folders, FolderView, "folder.json")}
+    %{data: render_many(folders, FolderView, "folder_excerpt.json")}
   end
 
   def render("show.json", %{folder: folder}) do
@@ -11,7 +11,25 @@ defmodule PhotogWeb.FolderView do
   end
 
   def render("folder.json", %{folder: folder}) do
+    %{
+      id: folder.id,
+      name: folder.name,
+      albums: Enum.map(folder.albums, &folder_album/1)
+    }
+  end
+
+  def render("folder_excerpt.json", %{folder: folder}) do
     %{id: folder.id,
       name: folder.name}
+  end
+
+  defp folder_album(album) do
+    %{
+      id: album.id,
+      name: album.name,
+      cover_image: %{
+        mini_thumbnail_path: album.cover_image.mini_thumbnail_path
+      }
+    }
   end
 end
