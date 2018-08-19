c1ca8aed22b499db2e1bb36d98e3481e2123dc90
Allen Garvey <allen.garvey@gmail.com>
2018-08-19 15:55:00 -0400

    Edit api album view to contain cover_image and images

--- lib/photog_web/views/album_view.ex
+++ lib/photog_web/views/album_view.ex
@@ -3,17 +3,25 @@ defmodule PhotogWeb.AlbumView do
   alias PhotogWeb.AlbumView
 
   def render("index.json", %{albums: albums}) do
-    %{data: render_many(albums, AlbumView, "album.json")}
+    %{data: render_many(albums, AlbumView, "album_excerpt.json")}
   end
 
   def render("show.json", %{album: album}) do
     %{data: render_one(album, AlbumView, "album.json")}
   end
 
+  def render("album_excerpt.json", %{album: album}) do
+    album_excerpt_to_map(album)
+  end
+
   def render("album.json", %{album: album}) do
     %{id: album.id,
       name: album.name,
-      folder_order: album.folder_order}
+      cover_image: PhotogWeb.ImageView.image_to_map(album.cover_image),
+      images: Enum.map(album.images, &PhotogWeb.ImageView.image_to_map/1),
+    }
+  end
+
   def album_excerpt_to_map(album) do
     %{
       id: album.id,
