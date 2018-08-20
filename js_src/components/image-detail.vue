bc5b6958796acf47772b3a946a9a2d6d2f16fd15
Allen Garvey <allen.garvey@gmail.com>
2018-08-20 16:53:49 -0400

    Add thumbnail list of related images for album images and person images

--- js_src/components/image-detail.vue
+++ js_src/components/image-detail.vue
@@ -1,8 +1,24 @@
 <template>
     <main class="main container" v-if="isModelLoaded">
+        <div class="album-image-show-header" v-if="parent">
+            <router-link :to="{name: parent.parentRouteName, params: {id: model.id}}">Back to {{model.name}}</router-link>
+            <div class="album-image-nav">
+                <div></div>
+                <a href="/albums/3596/images/14281#photo_id14281">Next</a>
+            </div>
+            <div class="album-image-nav-previews">
+                <ul class="image-preview-list">
+                    <li :id="`photo_id${image.id}`" :class="{'current-image': image.id === parent.modelId}" v-for="(image, i) in model.images" :key="i">
+                        <router-link :to="parent.showRouteFor(image)" class="preview-container">
+                            <img :src="thumbnailUrlFor(image.mini_thumbnail_path)">
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
         <div class="image-show-thumbnail-container">
             <a :href="masterUrl">
-                <img :src="thumbnailUrl"/>
+                <img :src="thumbnailUrlFor(image.thumbnail_path)"/>
             </a>
         </div>
         <div class="image-show-link-container">
@@ -12,9 +28,9 @@
             <h3 class="Info"></h3>
             <dl>
                 <dt>Date Taken</dt>
-                <dd>{{model.creation_time.formatted.us_date}} {{model.creation_time.formatted.time}}</dd>
+                <dd>{{image.creation_time.formatted.us_date}} {{image.creation_time.formatted.time}}</dd>
                 <dt>Favorite</dt>
-                <dd>{{model.is_favorite ? 'true' : 'false'}}</dd>
+                <dd>{{image.is_favorite ? 'true' : 'false'}}</dd>
             </dl>
         </div>
     </main>
@@ -43,17 +59,21 @@ export default {
     data() {
         return {
             model: null,
+            imageModel: null, //for when the model is the parent of the image
         }
     },
     computed: {
         isModelLoaded: function(){
-            return !!this.model;
-        },
-        thumbnailUrl: function(){
-            return this.generateImageUrl(this.model.thumbnail_path);
+            return this.model && this.image;
         },
         masterUrl: function(){
-            return this.generateImageUrl(this.model.master_path, false);
+            return this.generateImageUrl(this.image.master_path);
+        },
+        image: function(){
+            if(this.parent){
+                return this.imageModel;
+            }
+            return this.model;
         },
     },
     watch: {
@@ -63,13 +83,28 @@ export default {
     },
     methods: {
         loadModel: function(modelPath){
+            this.imageModel = null;
             this.getModel(modelPath).then((itemsJson)=>{
                 this.model = itemsJson;
+                
+                //if parent, lookup image in parent images
+                if(this.parent){
+                    const imageId = this.parent.modelId;
+                    for(let image of this.model.images){
+                        if(image.id === this.parent.modelId){
+                            this.imageModel = image;
+                            break;
+                        }
+                    }
+                }
+
             });
-        }, 
-        generateImageUrl: function(rawUrl, isThumbnail=true){
-            const imagePath = isThumbnail ? 'thumbnails' : 'images';
-            return `/media/${imagePath}/${encodeURI(rawUrl)}`;
+        },
+        thumbnailUrlFor: function(thumbnailPath){
+            return `/media/thumbnails/${encodeURI(thumbnailPath)}`;
+        },
+        generateImageUrl: function(rawUrl){
+            return `/media/images/${encodeURI(rawUrl)}`;
         },
     }
 }
