f870099cca7661736ff68b424d80247195b17de7
Allen Garvey <allen.garvey@gmail.com>
2018-09-24 23:32:53 -0400

    Add config to serve react build output

--- lib/photog_web/endpoint.ex
+++ lib/photog_web/endpoint.ex
@@ -7,9 +7,13 @@ defmodule PhotogWeb.Endpoint do
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
+  # plug Plug.Static,
+  #   at: "/", from: :photog, gzip: false,
+  #   only: ~w(css fonts images js favicon.ico robots.txt)
+
   plug Plug.Static,
-    at: "/", from: :photog, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/", from: {:photog, "priv/static/react"}, gzip: false,
+    only: ~w(static favicon.ico robots.txt index.html)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
