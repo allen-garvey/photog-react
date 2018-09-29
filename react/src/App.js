c400174d0e456dcbfd565407eb85e8ce80ec59c0
Allen Garvey <allen.garvey@gmail.com>
2018-09-28 23:44:45 -0400

    Add app header to react jsx

--- react/src/App.js
+++ react/src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header className="header container">
+          <h1 className="brand">Photog</h1>
+          <nav>
+            <ul callName="nav-list">
+              <li>Albums</li>
+              <li>Folders</li>
+              <li>Persons</li>
+              <li>Favorites</li>
+            </ul>
+          </nav>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
